b451843752dd4d8ea18b74689e30b100dfc78ba4
MD Anayet Hossen <mdsakayethossen2@gmail.com>
2025-07-28 20:45:02 +0600

    feat: add job matcher agent

---
+++ CareerPilot.API/Agents/JobMatcherAgent.cs
@@ -0,0 +1,35 @@
+﻿using CareerPilot.API.Plugins;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel;
+
+namespace CareerPilot.API.Agents
+{
+	public class JobMatcherAgent(Kernel kernel)
+	{
+		public const string AgentName = "JobMatcher";
+
+		public ChatCompletionAgent Create()
+		{
+			var getJobs = kernel.Plugins.GetFunction(nameof(JobAssistantPlugin), nameof(JobAssistantPlugin.GetJobsAsync));
+
+			return new ChatCompletionAgent
+			{
+				Name = AgentName,
+				Instructions = GetInstructions(),
+				Kernel = kernel,
+				Arguments = new KernelArguments(new PromptExecutionSettings()
+				{
+					FunctionChoiceBehavior = FunctionChoiceBehavior.Required([getJobs])
+				})
+			};
+		}
+
+		private static string GetInstructions()
+		{
+			return """
+               Get list of available jobs to find the best job matches based on a candidate's resume.
+               Be direct and professional. Do not engage in chitchat.
+               """;
+		}
+	}
+}
